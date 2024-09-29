308fd7ccc61ab53817575ccb1caea1bd907cd2ec
farran-77 <muhammadfarrab2017#gmail.com>
2024-09-29 21:12:14 +0500

    gig status

--- app/src/main/java/com/example/myapplication/ems3.java
+++ app/src/main/java/com/example/myapplication/ems3.java
@@ -16,11 +16,11 @@ import com.example.myapplication.R;
 
 import java.util.Objects;
 
-public class ems3 extends AppCompatActivity implements View.OnClickListener{
+public class ems3 extends AppCompatActivity {
 
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
-    public Cardview d1,d2,d3,d4 ;
+  //  public Cardview d1,d2,d3,d4 ;
 
     private static final int IMAGE_PICK_CODE = 1000; // Request code for image picker intent
     private ImageView imageView;
@@ -29,15 +29,15 @@ public class ems3 extends AppCompatActivity implements View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ems3);
-         d1 = (Cardview) findViewById(R.id.d1);
-        d2 = (Cardview) findViewById(R.id.d2);
-        d3 = (Cardview) findViewById(R.id.d3);
-        d4 = (Cardview) findViewById(R.id.d4);
+       //  d1 = (Cardview) findViewById(R.id.d1);
+       // d2 = (Cardview) findViewById(R.id.d2);
+        //d3 = (Cardview) findViewById(R.id.d3);
+       // d4 = (Cardview) findViewById(R.id.d4);
 
-        d1.setOnClickListener((View.OnClickListener) this);
-        d2.setOnClickListener((View.OnClickListener) this);
-        d3.setOnClickListener((View.OnClickListener) this);
-        d4.setOnClickListener((View.OnClickListener) this);
+        //d1.setOnClickListener((View.OnClickListener) this);
+        //d2.setOnClickListener((View.OnClickListener) this);
+        //d3.setOnClickListener((View.OnClickListener) this);
+        //d4.setOnClickListener((View.OnClickListener) this);
 
 
 
@@ -63,15 +63,14 @@ public class ems3 extends AppCompatActivity implements View.OnClickListener{
         }
         return super.onOptionsItemSelected(item);
     }
-@SuppressLint("NonConstantResourceId")
-public void onClick(View v) {
-    Intent i;
-    switch (v.getId()) {
-        case R.id.d1; new Intent(this,d1.class); startActivity(i); break;
-        case R.id.d2; new Intent(this,d2.class); startActivity(i); break;
-        case R.id.d3; new Intent(this,d3.class); startActivity(i); break;
-        case R.id.d4; new Intent(this,d4.class); startActivity(i); break;
+//public void onClick(View v) {
+    //Intent i;
+    //switch (v.getId()) {
+        //case R.id.d1; new Intent(this,d1.class); startActivity(i); break;
+        //case R.id.d2; new Intent(this,d2.class); startActivity(i); break;
+        //case R.id.d3; new Intent(this,d3.class); startActivity(i); break;
+        //case R.id.d4; new Intent(this,d4.class); startActivity(i); break;
 
     }
-}
-}
+//}
+//}
